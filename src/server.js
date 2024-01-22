8faf1bd826a3cbd401fa5b324f14cd8f386696b5
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-22 19:56:50 -0300

    ✨ get data from req

--- src/server.js
+++ src/server.js
@@ -2,9 +2,18 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-  console.log(method, url);
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    req.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     return res
@@ -13,10 +22,11 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name: name,
+      email: email,
     });
 
     return res.writeHead(201).end();
