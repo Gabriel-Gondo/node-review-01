6b13687527d43afc92c1c046742f1d787fb5abe3
Gabriel Gondo <gabrielgondo.dev@gmail.com>
2024-01-22 19:46:46 -0300

    ✨ await stream end

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -10,8 +10,19 @@ class InverseNumberStream extends Transform {
     callback(null, Buffer.from(String(transformed)));
   }
 }
-const server = http.createServer((req, res) => {
-  return req.pipe(new InverseNumberStream()).pipe(res);
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+  console.log(fullStreamContent);
+
+  return res.end(fullStreamContent);
+
+  // return req.pipe(new InverseNumberStream()).pipe(res);
 });
 
 server.listen(3334);
